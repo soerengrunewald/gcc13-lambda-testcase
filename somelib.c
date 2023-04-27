1ea6b38de0f08ac30a43f5757f151915da295673
Soeren Grunewald <soeren.grunewald@desy.de>
2023-04-27 15:26:46 +0200

    Do something with the unknown data

--- somelib.c
+++ somelib.c
@@ -4,9 +4,14 @@
 
 #define DEFAULTSIZE 10
 
+struct TYPE {
+	char data[DEFAULTSIZE];
+};
+
+
 SOMETYPE* sometype_create(void)
 {
-	return malloc(DEFAULTSIZE);
+	return calloc(1, sizeof(struct TYPE));
 }
 
 void sometype_destroy(SOMETYPE* st)
@@ -16,9 +21,29 @@ void sometype_destroy(SOMETYPE* st)
 
 int sometype_do_something(SOMETYPE* st, int what)
 {
-	if (st) {
-		memset(st, what, DEFAULTSIZE);
+	struct TYPE* t = (struct TYPE*)st;
+	if (t) {
+		memset(&t->data, what, DEFAULTSIZE);
+		t->data[DEFAULTSIZE-1] = 0;
 		return 0;
 	}
 	return -1;
+}
+
+const char* sometype_to_string(SOMETYPE* st, const char* prefix)
+{
+	static char buf[256];
+	struct TYPE* t = (struct TYPE*)st;
+	char* ptr = NULL;
+
+	if (t) {
+		memset(buf, 0, sizeof(buf));
+		ptr = strcpy(buf, prefix);
+
+		for (int i=0; i!=DEFAULTSIZE; ++i) {
+			*ptr++ = t->data[i] + 'a';
+		}
+	}
+	buf[255] = 0;
+	return buf;
 }
--- somelib.h
+++ somelib.h
@@ -12,6 +12,8 @@ void sometype_destroy(SOMETYPE*);
 
 int sometype_do_something(SOMETYPE*, int);
 
+const char* sometype_to_string(SOMETYPE*, const char*);
+
 #ifdef  __cplusplus
 }
 #endif
